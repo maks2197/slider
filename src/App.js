1bd80a1cd10c8f8f199d46a821072c3f9e7be3e1
maks <maks2197@gmail.com>
2018-07-08 21:59:37 +1100

    сделал верстку 1-го дня

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Menu from './components/Menu';
 
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
 const HeaderWrapper = styled.header`
   width: 100%;
-  height: 49PX;
+  height: 49px;
   background-color: #242424;
   opacity = 0.8;
 `
-
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repiat;
+  background-size: cover;
+`
 class App extends Component {
   render() {
     return (
@@ -24,21 +35,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-         <div className="wrapper">
-         <SayFullName name="Sergey" surname="Petrov" link="vk.com" />
-         <SayFullName name="Marina" surname="Polischuk" link="indeed.com" />
-         <SayFullName name="Alex" surname="Siluanov" link="mail.ru" />
-         </div>
+      
          <div className="App">
           <HeaderWrapper>
             <Grid>
-              <Header/>
+              <Header />
             </Grid>
           </HeaderWrapper>
-          <Main/>
-          <Menu/>      
-
-         </div>
+          <MenuWrapper>
+            <Grid>
+              <Menu />
+            </Grid>
+          </MenuWrapper>
+          <MainWrapper>
+            <Grid>
+              <Main />
+            </Grid>
+          </MainWrapper>
+          </div>
 
 
 
